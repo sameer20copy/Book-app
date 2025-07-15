f75e327636a33d28dee64f6355bf76896f72d5b8
Sameer Sharma <sameer20copy@gmail.com>
2025-07-15 15:10:04 +0530

    set cors

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,7 @@ const cors=require("cors");
 const mongoose=require("mongoose");
 const userRouter=require("./routes/routes")
 
-app.use(cors({
-  origin: "https://fanciful-semifreddo-5af986.netlify.app/", // Replace with your frontend Netlify URL
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}))
+app.use(cors());
 app.use(express.json())
 mongoose.connect("mongodb+srv://Books:as816@books.4fwjwy3.mongodb.net/?retryWrites=true&w=majority&appName=Books").then(console.log("Mongodb is connected"))
 app.use("/",userRouter);
